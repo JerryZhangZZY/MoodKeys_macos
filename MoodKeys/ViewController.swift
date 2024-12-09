79b0c8e52b939c2f0000ac29903d55552c99cb4e
zhangzeyu07 <zhangzeyu07@baidu.com>
2024-12-09 10:41:02 +0800

    Initial Commit

---
+++ MoodKeys/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MoodKeys
+//
+//  Created by zhangzeyu07 on 2024/12/9.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
